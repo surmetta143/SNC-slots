f431b0f93ec377928be0a057fb315512fbe971b9
surmetta143 <90646511+surmetta143@users.noreply.github.com>
2026-07-05 03:29:28 +0530

    Create courses.js

---
+++ src/courses.js
@@ -0,0 +1,35 @@
+export const COURSES = [
+  {
+    id: "python-pytest",
+    title: "Automation Test Cases with Python & Pytest",
+    description:
+      "Learn to write automation test cases using Selenium with Python and the Pytest framework.",
+    videos: 7,
+    playlistUrl:
+      "https://www.youtube.com/playlist?list=PLFqCDCzADk76DvMwKzHYDeNzD443Yj4F1",
+    thumbnail: "https://img.youtube.com/vi/0/hqdefault.jpg",
+  },
+  {
+    id: "java-testng",
+    title: "Automation Test Cases with Selenium, Java & TestNG",
+    description:
+      "Write automation test cases using Selenium with Java and the TestNG framework.",
+    videos: 6,
+    playlistUrl:
+      "https://www.youtube.com/playlist?list=PLFqCDCzADk777ZvjE1pm9uAcjnP4TEi7K",
+    thumbnail: "https://img.youtube.com/vi/0/hqdefault.jpg",
+  },
+  {
+    id: "manual-testing",
+    title: "Manual Testing Classes",
+    description:
+      "Agile, sprint planning, story assignment, and manual test case execution explained.",
+    videos: 4,
+    playlistUrl:
+      "https://www.youtube.com/playlist?list=PLFqCDCzADk763xqL6cvF74N2--bcAOvYT",
+    thumbnail: "https://img.youtube.com/vi/0/hqdefault.jpg",
+  },
+];
+
+export const COURSE_PRICE = 99;
+export const UPI_ID = "8985256492@ybl";
